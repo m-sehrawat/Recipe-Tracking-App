5766e34eda974b3fc4055bfdfd6c935340ab1801
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-01-03 15:35:57 +0530

    server & API request added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Recipe } from './components/Recipe';
+import { Recipe } from './components/RecipeInput'
 
 function App() {
     return (
--- src/components/RecipeInput.jsx
+++ src/components/RecipeInput.jsx
@@ -1,17 +1,97 @@
-export const RecipeInput = () => {
+import { RecipeCard } from "./RecipeCard";
+
+import { useEffect, useState } from "react";
+
+export const Recipe = () => {
+
+    const initState = { title: "", ingredients: "", cookTime: "", imgURL: "", instructions: "" };
+
+    const [recipeData, setRecipeData] = useState(initState);
+    const [form, setForm] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = () => {
+
+        fetch(`http://localhost:3005/recipe`)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                setForm(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    const postData = (data) => {
+
+        fetch(`http://localhost:3005/recipe`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                getData();
+            })
+    }
+
+
+    const handleChange = (e) => {
+        let { name, value } = e.target;
+
+        setRecipeData({ ...recipeData, [name]: value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        postData(recipeData);
+    }
+
+
+
     return (
-        <div className="mx-3 mt-3 border p-4">
-            <p className="text-center fs-5">Create a new recipe</p>
-            <form>
-                <input type="text" className="form-control mb-3" placeholder="Title" />
-                <input type="text" className="form-control mb-3" placeholder="Ingredients" />
-                <input type="text" className="form-control mb-3" placeholder="Time to cook" />
-                <input type="text" className="form-control mb-3" placeholder="Image URL" />
-                <input type="text" className="form-control mb-3" placeholder="Instructions" />
-                <div className="d-grid">
-                <input type="submit" className="btn btn-success" />
+
+        <div className='mainBox'>
+            <div className="container">
+                <div className="row">
+                    <div className="col boxHeightTop">
+
+                        <div className="mx-3 mt-3 border p-4" >
+                            <p className="text-center fs-5">Create a new recipe</p>
+                            <form onSubmit={handleSubmit}>
+                                <input onChange={handleChange} name="title" type="text" className="form-control mb-3" placeholder="Title" />
+                                <input onChange={handleChange} name="ingredients" type="text" className="form-control mb-3" placeholder="Ingredients" />
+                                <input onChange={handleChange} name="cookTime" type="text" className="form-control mb-3" placeholder="Time to cook" />
+                                <input onChange={handleChange} name="imgURL" type="text" className="form-control mb-3" placeholder="Image URL" />
+                                <input onChange={handleChange} name="instructions" type="text" className="form-control mb-3" placeholder="Instructions" />
+                                <div className="d-grid">
+                                    <input type="submit" className="btn btn-success" />
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                    <div className="col boxHeightTop cardBox">
+                        <RecipeCard />
+                    </div>
                 </div>
-            </form>
+                <div className="row boxHeightBottom">3</div>
+            </div>
         </div>
+
+
+
     )
 }
